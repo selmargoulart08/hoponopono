9fcc0e05262a114c31b3382b6fa6c52741c7d473
devSelmaJr <60160024+selmargoulart08@users.noreply.github.com>
2025-09-17 09:48:31 -0300

    Commit

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'hooponopono-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/estilo.css',
+  '/script.js',
+  '/music.mp3',
+  '/lotus.ico'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Cache opened');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
