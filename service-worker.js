c39e5a31a8b5d5d8253a71c0b207db281715ab1d
devSelmaJr <60160024+selmargoulart08@users.noreply.github.com>
2025-09-17 10:34:22 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,42 @@
-const CACHE_NAME = 'hooponopono-cache-v1';
-const urlsToCache = [
+// This is the "Offline page" service worker
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+// Lista de arquivos para pré-caching
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+
+const urlsToPrecache = [
   '/',
-  '/index.html',
-  '/estilo.css',
-  '/script.js',
-  '/music.mp3',
-  '/lotus.ico'
+  'index.html',
+  'estilo.css',
+  'script.js',
+  'music.mp3',
+  'lotus.ico',
+  'logo-192.png',
+  'logo-512.png',
+  'manifest.json',
+  'offline.html'
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Cache opened');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
+workbox.precaching.precacheAndRoute(urlsToPrecache);
+
+// Regra de fallback para quando o usuário estiver offline
+const offlineFallbackPage = 'offline.html';
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      (async () => {
+        try {
+          const networkResponse = await fetch(event.request);
+          return networkResponse;
+        } catch (error) {
+          const cache = await caches.open(workbox.core.cacheNames.precache);
+          const cachedResponse = await cache.match(offlineFallbackPage);
+          return cachedResponse;
         }
-        return fetch(event.request);
-      })
-  );
+      })()
+    );
+  }
 });
